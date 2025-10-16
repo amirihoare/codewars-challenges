d1e97f5cecbcefb682ceb212bf3512868837e86a
Amiri Hoare <amirihoare07@gmail.com>
2025-10-16 16:08:27 -0600

    find highest scoring word
    https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript

---
+++ 6th-kyu/highWord.js
@@ -0,0 +1,30 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+
+// https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
+
+function high(x) {
+  const words = x.split(' ');
+  let highestScore = 0;
+  let highestWord = '';
+
+  for (const word of words) {
+    const score = [...word].reduce((acc, char) => acc + (char.charCodeAt(0) - 96), 0);
+    if (score > highestScore) {
+      highestScore = score;
+      highestWord = word;
+    }
+  }
+
+  return highestWord;
+}
